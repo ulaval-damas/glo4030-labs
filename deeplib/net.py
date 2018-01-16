3d88d2c9bcaab1ae116dd20aa586752e1254717d
mcarpentier <mathieu.carpentier.3@ulaval.ca>
2018-01-16 13:18:11 -0500

    Added lab 1

---
+++ deeplib/net.py
@@ -0,0 +1,71 @@
+# Adapted from
+# http://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MnistNet(nn.Module):
+
+    def __init__(self):
+        super(MnistNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, 3, padding=1)
+        self.conv2 = nn.Conv2d(10, 50, 3, padding=1)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(50 * 7 * 7, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    @staticmethod
+    def num_flat_features(x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+class CifarNet(nn.Module):
+    def __init__(self):
+        super(CifarNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, 3, padding=1)
+        self.conv2 = nn.Conv2d(10, 50, 3, padding=1)
+        self.conv3 = nn.Conv2d(50, 150, 3, padding=1)
+        self.conv4 = nn.Conv2d(150, 300, 3, padding=1)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(300 * 8 * 8, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    @staticmethod
+    def num_flat_features(x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
