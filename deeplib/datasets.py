922d130f7580571cc84cfbc00e6045f297f02623
mcarpentier <mathieu.carpentier.3@ulaval.ca>
2018-01-18 14:20:20 -0500

    Fix dataset path for lab

--- deeplib/datasets.py
+++ deeplib/datasets.py
@@ -3,12 +3,12 @@ from torchvision.datasets.cifar import CIFAR10
 
 
 def load_mnist():
-    train_dataset = MNIST('dataset', train=True, download=True)
-    test_dataset = MNIST('dataset', train=False, download=True)
+    train_dataset = MNIST('/rap/colosse-users/GLO-4030/datasets/mnist', train=True, download=False)
+    test_dataset = MNIST('/rap/colosse-users/GLO-4030/datasets/mnist', train=False, download=False)
     return train_dataset, test_dataset
 
 
 def load_cifar10():
-    train_dataset = CIFAR10('dataset', train=True, download=True)
-    test_dataset = CIFAR10('dataset', train=False, download=True)
+    train_dataset = CIFAR10('/rap/colosse-users/GLO-4030/datasets/cifar10', train=True, download=False)
+    test_dataset = CIFAR10('/rap/colosse-users/GLO-4030/datasets/cifar10', train=False, download=False)
     return train_dataset, test_dataset
