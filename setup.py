bc0107c764bbcba1efbc6ed602b0ab1cf3ef6736
Frédérik Paradis <fredy_14@live.fr>
2020-12-10 15:28:05 -0500

    Add requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from distutils.core import setup
 setup(
     name='deeplib',
     version='0.1',
-    packages=['deeplib']
+    packages=['deeplib'],
+    install_requires=['torch', 'torchvision', 'torchtext', 'torchaudio', 'pandas', 'ipykernel', 'matplotlib',
+                      'scikit-learn', 'graphviz', 'ipython', 'gensim', 'numpy', 'scipy', 'Pillow'],
 )
